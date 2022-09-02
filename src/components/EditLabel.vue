83e91d8c6be6fc73ae24118dce18ce800675fc53
Lily <2542101862@qq.com>
2022-09-02 16:59:30 +0800

    在EditLabel用vuex

--- src/components/EditLabel.vue
+++ src/components/EditLabel.vue
@@ -23,16 +23,17 @@
     import {Component} from 'vue-property-decorator';
     import FormItem from '@/components/FormItem.vue';
     import Button from '@/components/Button.vue';
-    import store from '@/store/index2';
     @Component({
         components: {Button, FormItem}
     })
     export default class EditLabel extends Vue {
-        tag?: Tag = undefined
+        get tag() {
+            return this.$store.state.currentTag;
+        }
 
         created() {
-            // TODO
-            // this.tag =  // store.findTag(this.$route.params.id);
+            const id = this.$route.params.id;
+            this.$store.commit('setCurrentTag', id);
             if (!this.tag) {
                 this.$router.replace('/404') //路由器跳转
             }
